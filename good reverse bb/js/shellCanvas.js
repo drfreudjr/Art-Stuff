f95555b1920d9a9ada6799671957cde95e0e0360
DrFreudJr <drfreudj88@gmail.com>
2021-03-20 19:46:48 -0400

    reverse bb

---
+++ good reverse bb/js/shellCanvas.js
@@ -0,0 +1,79 @@
+const cl = console.log;
+
+window.onload = function () {           // onload wrapper
+                                        // Global 2D context reference
+var canvas;                             // Global canvas object reference
+var context; 
+
+
+
+// // Begin dynamic fulls screen canvas code
+
+sizeCanvas()                            // create initial canvas
+addEventListener("resize", sizeCanvas); // resize canvas and redraw on window size change
+
+function createCanvas () {   
+    const canvas = document.createElement("canvas"); 
+    canvas.style.position = "absolute"; 
+    canvas.style.left     = "0px";      
+    canvas.style.top      = "0px";
+
+    document.body.appendChild(canvas);  // Add to document
+    return canvas;
+}
+
+function sizeCanvas () {                // Create or resize 
+    if (canvas === undefined) {         
+        canvas = createCanvas();        
+        context = canvas.getContext("2d");  
+    }
+    canvas.width  = innerWidth; 
+    canvas.height = innerHeight; 
+    drawScreen()     
+}
+
+function drawScreen() {  // wrapper that gets called on resize event
+
+    //  //  // Enter Page Specific Code here
+
+let width = 0
+let height = 0
+let iteration = 1
+
+    let r = 0
+    let g = 0
+    let b = 0
+
+    let iterations = 0
+
+function crazyLines () {
+    iterations++
+
+
+    if (r<255) r+=1
+
+    width = width +1.5
+    height = height +.6
+    iteration++
+    cyclesPerFrame = 100000
+
+    for (let i = 0; i < cyclesPerFrame; ++i) {
+        cyclesPerFrame=cyclesPerFrame- 2000
+
+        context.strokeStyle = `rgb(${r},${g},${b})`
+        x = Math.round(Math.random()*width*2)
+        y = Math.round(Math.random()*height*2)
+        dx = Math.round(Math.random()*innerWidth-width*2)
+        dy = Math.round(Math.random()*innerHeight-height*2)
+
+        context.beginPath()
+        context.moveTo(x,y)
+        context.lineTo(dx,dy)
+        context.stroke()
+    }
+    if (iterations < 300)
+        requestAnimationFrame(crazyLines)
+}
+crazyLines()
+}   // end drawScreen wrapper
+}   // end onload wrapper
